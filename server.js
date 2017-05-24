ae7c6f8a5db109606e8f4c7c2165288ea6b63bd2
Karolin Rafalski <kr2174@caa.columbia.edu>
2017-05-24 12:38:36 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -52,5 +52,20 @@ app.delete ('/todos/:id' , db.deleteTodo, ( req , res ) => {
   res.send ( 'ok' );
 });
 
+//heroku deployment
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 //listen
 app.listen ( port , () => console.log( 'Get things done on port' , port , '//' , new Date() ) );
